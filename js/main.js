5098e94fa425da35930e2bb72d7f7268016fdcdc
ij0b <ij0b@mail.ru>
2021-05-19 15:58:11 +0300

    Homework 3 done

--- js/main.js
+++ js/main.js
@@ -1,183 +1,133 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
 class ProductsList {
     constructor(container = '.products') {
         this.container = container;
-        this.goods = [];
-        this._fetchProducts();
+        this.goods = [];//массив товаров
+        this.allProducts = [];//массив объектов
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = [...data];
+                this.render()
+            });
     }
-
-    _sumPrices() {
-        let sumPrices = 0;
-        for (let product of this.goods) {
-            sumPrices += product.price;
-        }
-        console.log("+++2.\n Сумма всех товаров: " + sumPrices + "\n");
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
-
-    _fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Notebook', price: 2000, img: "img/3x3.jpg" },
-            { id: 2, title: 'Mouse', price: 20, img: "img/3x1.jpg" },
-            { id: 3, title: 'Keyboard', price: 200, img: "img/1x3.jpg" },
-            { id: 4, title: 'Gamepad', price: 50 },
-            { id: 5, title: 'Graphics card', price: 150, img: "img/3x3.jpg" },
-            { id: 6, title: 'Personal computer number 123/200 long name ++++++++++++++++++', price: 500, img: "img/3x3.jpg" },
-        ];
+    calcSum() {
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
-
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
             const productObj = new ProductItem(product);
-            block.insertAdjacentHTML('beforeend', productObj.render())
-            //            block.innerHTML += productObj.render();
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
-    }
 
+    }
 }
 
 
 class ProductItem {
-    constructor(product) {
-        let { title, price, id, img } = product;
-        if (img == undefined) {
-            img = img = 'https://via.placeholder.com/200x150';
-        }
-        this.title = title;
-        this.price = price;
-        this.id = id;
+    constructor(product, img = 'img/3x1.jpg') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
-
     render() {
         return `<div class="product-item" data-id="${this.id}">
-                <a class="product_image-container" href="">
-                    <img class="product_image" src="${this.img}" alt="${this.title}">
-                </a>
-                <h3 class="product_caption">
-                    <a class="product_caption-link" href="">
-                        ${this.title}
-                    </a>    
-                </h3>
-                <div class="product_price-btn-container">
-                    <p class="product_price">${this.price}</p>                    
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
                     <button class="buy-btn">Купить</button>
-                </div>    
+                </div>
             </div>`
     }
 }
 
 let list = new ProductsList();
-list.render();
-list._sumPrices();
+
 
 class ListBasketGoods {
-    constructor() {
-
+    constructor(container = '.basket') {
+        this.container = container;
+        this.goods = [];//массив товаров
+        this.allProducts = [];//массив объектов
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = [...data.contents];
+                this.render()
+            });
     }
 
-    show() {
-
+    _getProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
-
-    hide() {
-
+    calcSum() {
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
-
-    sort() {
-
-    }
-
-    filter() {
-
-    }
-
-    delete() {
-
-    }
-}
-
-class BasketProduct {
-    constructor() {
-
-    }
-
-    add() {
-
-    }
-
-    delete() {
-
-    }
-
-    showDetailPage() {
-
-    }
-
-    changeQuantity() {
-
-    }
-}
-
-
-/*
-    +++3. *Некая сеть фастфуда предлагает несколько видов гамбургеров:
-    ### Маленький (50 рублей, 20 калорий).
-    ### Большой (100 рублей, 40 калорий).
-    Гамбургер может быть с одним из нескольких видов начинок (обязательно):
-    С сыром (+10 рублей, +20 калорий).
-    С салатом (+20 рублей, +5 калорий).
-    С картофелем (+15 рублей, +10 калорий).
-    Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий).
-    Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Можно использовать примерную архитектуру класса из методички, но можно использовать и свою.
-*/
-
-class Hamburger {
-    constructor() {
-        this.listHamburgers = [
-            { name: "маленький", price: 50, calories: 20 },
-            { name: "большой", price: 100, calories: 40 }
-        ]
-        this.listFillings = [
-            { name: "сыр", price: 10, calories: 20 },
-            { name: "салат", price: 20, calories: 5 },
-            { name: "картофель", price: 15, calories: 10 },
-        ]
-        this.listAdditionalFillings = [
-            { name: "приправа", price: 15, calories: 0 },
-            { name: "майонез", price: 20, calories: 5 },
-        ]
-    }
-
-    calculationСostAndCalorieAllHamburgers() {
-        console.log("+++3.\n");
-        for (let hamburger of this.listHamburgers) {
-            for (let filling of this.listFillings) {
-                let completedHamburger = `
-                    Гамбургер ${hamburger.name}, ${filling.name} \n
-                    Цена: ${hamburger.price + filling.price}. Каллорийность: ${hamburger.calories + filling.calories} \n
-                `;
-                console.log(completedHamburger);
-                for (let additionalFilling of this.listAdditionalFillings) {
-                    completedHamburger = `
-                        Гамбургер ${hamburger.name}, ${filling.name}, ${additionalFilling.name} \n
-                        Цена: ${hamburger.price + filling.price + additionalFilling.price}. Каллорийность: ${hamburger.calories + filling.calories + additionalFilling.calories} \n
-                    `;
-                    console.log(completedHamburger);
-                }
-            }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new BasketProduct(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
     }
 }
 
-let objHamburger = new Hamburger();
-objHamburger.calculationСostAndCalorieAllHamburgers();
 
-/*
-    ---3. *Некая сеть фастфуда предлагает несколько видов гамбургеров:
-*/
+class BasketProduct {
+    constructor(product, img = 'img/3x1.jpg') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+        this.quantity = product.quantity;
+    }
+    render() {
+        return `<div class="basket-product-item" data-id="${this.id}">
+                <img class="basket-product-img" src="${this.img}" alt="Some img">
+                <h3 class="basket-product-name">${this.title}</h3>
+                <p class="basket-product-price">${this.price} $</p>
+                <p class="basket-product-quantity">Количество -${this.quantity}</p>
+                <button class="basket-product-btn">x</button>
+            </div>`
+    }
+}
 
-
-
-
-
-
+let basketList = new ListBasketGoods();
